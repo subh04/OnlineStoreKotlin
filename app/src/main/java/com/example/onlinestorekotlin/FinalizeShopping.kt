9d5f33cfaa288d18a4172b505cbbfa289fa4696e
Subhasish <subhasish469@gmail.com>
2020-06-05 20:13:19 +0530

    PayPal payment integration

--- app/src/main/java/com/example/onlinestorekotlin/FinalizeShopping.kt
+++ app/src/main/java/com/example/onlinestorekotlin/FinalizeShopping.kt
@@ -1,15 +1,25 @@
 package com.example.onlinestorekotlin
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.RequestFuture
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.paypal.android.sdk.payments.PayPalConfiguration
+import com.paypal.android.sdk.payments.PayPalPayment
+import com.paypal.android.sdk.payments.PayPalService
+import com.paypal.android.sdk.payments.PaymentActivity
 import kotlinx.android.synthetic.main.activity_finalize_shopping.*
+import java.math.BigDecimal
 
 class FinalizeShopping : AppCompatActivity() {
+
+    var totalPrice:Long=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +31,8 @@ class FinalizeShopping : AppCompatActivity() {
         var requestQ=Volley.newRequestQueue(this@FinalizeShopping)
         var stringRequest=StringRequest(Request.Method.GET,calculateTotalPriceURL,Response.Listener { response ->
 
-            btnPaymentProcessing.text="Pay Rs."+response+" via PayPal Now!"
+            btnPaymentProcessing.text="Pay $."+response+" via PayPal Now!"
+            totalPrice=response.toLong()
 
 
         },Response.ErrorListener { error ->
@@ -33,6 +44,47 @@ class FinalizeShopping : AppCompatActivity() {
         requestQ.add(stringRequest)
 
 
+        var paypalConfig:PayPalConfiguration=PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX).clientId(MyPayPal.clientID)
+        var ppService = Intent(this@FinalizeShopping,PayPalService::class.java)
+        ppService.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,paypalConfig)
+        startService(ppService)
 
+        btnPaymentProcessing.setOnClickListener {
+
+            var ppProcessing=PayPalPayment(BigDecimal.valueOf(totalPrice),
+                "USD","Online E commerce App Kotlin ",
+                PayPalPayment.PAYMENT_INTENT_SALE)
+
+            var paypalPaymentIntent=Intent(this,PaymentActivity::class.java)
+            paypalPaymentIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,paypalConfig)
+            paypalPaymentIntent.putExtra(PaymentActivity.EXTRA_PAYMENT,ppProcessing)
+            startActivityForResult(paypalPaymentIntent,1000)
+
+
+
+        }
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==1000){
+            if (resultCode==Activity.RESULT_OK){
+                var intent=Intent(this,ThankyouActivity::class.java)
+                startActivity(intent)
+            }else{
+
+                Toast.makeText(this,"Sorry! something went wrong. Try again",Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(Intent(this,PayPalService::class.java))
     }
 }
---
+++ app/src/main/java/com/example/onlinestorekotlin/MyPayPal.kt
@@ -0,0 +1,7 @@
+package com.example.onlinestorekotlin
+
+class MyPayPal {
+    companion object{
+        var clientID="AWVAqqfwN3_3tJj1Wq20taRlc46bDUK0Pt-gUeUljdp-Uo-Bx9uUbNXx0UoQmn3cCoFdnUFjxqcWVA5z"
+    }
+}
